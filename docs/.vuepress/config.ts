c78192dff7813c6758bec300fb0082c0cfaaaf85
wxvirus <1418667580@qq.com>
2022-06-19 02:05:50 +0800

    update confit

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -2,7 +2,6 @@ import { defineUserConfig } from 'vuepress'
 import theme from './theme'
 
 export default defineUserConfig({
-    dest: 'dist',
     lang: 'zh-CN',
     title: 'wxvirus',
     description: 'wxvirus blog and note',
